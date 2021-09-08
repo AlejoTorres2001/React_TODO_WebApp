b7248144d1466a056719f9c8795a9f6f56499012
T0w3r <torres.alejo2001@gmail.com>
2021-09-08 11:26:29 -0300

    fixing marging

--- src/components/pages/About/AboutPage.js
+++ src/components/pages/About/AboutPage.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export default function AboutPage() {
   return (
     <>
-      <div className="container text-center my-2 w-100 text-white">
+      <div className="container text-center my-1 w-100 text-white">
         <h1 className="fs-1 text-white font">How does it works ?</h1>
         <h6 class=" text-white font">It´s very simple :)</h6>
       </div>
       <div className="container text-center my-1 w-50 text-white">
         <ol class=" list-group my-4 text-white">
           <Link to="/login" className="text-decoration-none">
-            <li class=" list-group-item list-group my-3 list-unstyled text-white bg">
+            <li class=" list-group-item list-group my-1 list-unstyled text-white bg">
               <p className="text fs-4">
                 Register your account here !
                 <i class="bi bi-box-arrow-in-right fs-2"></i>
@@ -19,7 +19,7 @@ export default function AboutPage() {
             </li>
           </Link>
           <Link to="/login" className="text-decoration-none">
-            <li class="list-group-item my-3 list-unstyled text-white bg">
+            <li class="list-group-item my-1 list-unstyled text-white bg">
               {" "}
               <p className="text fs-4">Login with your new account </p>{" "}
               <i class="bi bi-person-circle fs-2"></i>
@@ -27,7 +27,7 @@ export default function AboutPage() {
           </Link>
 
           <Link to="/" className=" text-decoration-none">
-            <li class="list-group-item my-3 list-unstyled text-white bg">
+            <li class="list-group-item my-1 list-unstyled text-white bg">
               {" "}
               <p className="text fs-4">
                 Manage all your daily tasks here
