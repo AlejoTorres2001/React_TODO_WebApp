9916061929a396b8166590467f5e0c1af5e41d74
T0w3r <torres.alejo2001@gmail.com>
2021-09-08 11:39:19 -0300

    fix

--- src/components/pages/About/AboutPage.js
+++ src/components/pages/About/AboutPage.js
@@ -7,10 +7,10 @@ export default function AboutPage() {
       <div className="container text-center my-1 w-100 text-white">
         <h1 className="fs-1 text-white font">How does it works ?</h1>
       </div>
-      <div className="container text-center w-50 text-white">
+      <div className="container text-center w-75 text-white">
         <ol class=" list-group my-2 text-white">
           <Link to="/login" className="text-decoration-none">
-            <li class=" list-group-item list-group my-1 list-unstyled text-white bg">
+            <li class=" list-group-item my-1 list-unstyled text-white bg">
               <p className="text fs-4">
                 Register your account here !
                 <i class="bi bi-box-arrow-in-right fs-2"></i>
@@ -19,15 +19,14 @@ export default function AboutPage() {
           </Link>
           <Link to="/login" className="text-decoration-none">
             <li class="list-group-item my-1 list-unstyled text-white bg">
-              {" "}
-              <p className="text fs-4">Login with your new account </p>{" "}
+             
+              <p className="text fs-4">Login with your new account </p>
               <i class="bi bi-person-circle fs-2"></i>
             </li>
           </Link>
 
           <Link to="/" className=" text-decoration-none">
             <li class="list-group-item my-1 list-unstyled text-white bg">
-              {" "}
               <p className="text fs-4">
                 Manage all your daily tasks here
                 <i class="bi bi-list-task fs-1"></i>
